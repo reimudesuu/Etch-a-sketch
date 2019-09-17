cd24ec48806b3bc124be14080ccc6002b8fcec1b
reimudesuu <tosunaliatikdev@gmail.com>
2019-09-17 19:45:55 +0300

    rainbowtime and fixes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector("#container")
-black = true;
+let black = true;
 
 
 
@@ -28,9 +28,9 @@ container.style.border ="0.2em inset black";
 container.style.border = "inset";
 
 const tiles = document.getElementsByClassName("newDiv")
-
+//paint squares black on mouseover effect if black is true
 function tileBlack(){
-    if(black){
+    if(black == true){
         for(tile of tiles){
             tile.addEventListener("mouseover",function(event){
             event.target.style.backgroundColor = "black";})
@@ -46,8 +46,8 @@ function tileBlack(){
 tileBlack()
 
 function tileClear(){
-    for(tile of tiles){
-    const button = document.querySelector("#clear");
+    for(let tile of tiles){
+        const button = document.querySelector("#clear");
     button.addEventListener('click', event => {
     tile.style.backgroundColor = "white";
     })
@@ -55,3 +55,31 @@ function tileClear(){
     
     
     tileClear();
+
+
+//make rainbow button capable of changing black variable to false. if it is false paint squares with random generated colours.
+function rainbowTile(){
+    black = false;
+    const rainbow = document.querySelector("#rainbow")
+    rainbow.addEventListener('click', event => {
+        if(black !== true){    
+            for(tile of tiles){
+                
+                let randomnum = Math.floor(Math.random()*255)
+                let randomnum2 = Math.floor(Math.random()*255)
+                let randomnum3 = Math.floor(Math.random()*255)
+                tile.addEventListener("mouseover",function(event){
+                event.target.style.backgroundColor = `rgb(${randomnum}, ${randomnum2}, ${randomnum3})`;})}
+    }
+})}
+//make black button capable of changing black variable to true"
+function blackButton(){
+     const paintitblack = document.querySelector("#black")
+     paintitblack.addEventListener('click', event => {
+     location.reload()
+    })
+   }
+rainbowTile();
+blackButton();
+
+
